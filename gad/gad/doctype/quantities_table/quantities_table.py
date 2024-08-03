c5411574cf23e64fe3a33fdd9a66431b666cc723
Gadallah <ahmedgadallah1899@gmail.com>
2024-08-03 19:24:27 +0300

    ddd

---
+++ gad/gad/doctype/quantities_table/quantities_table.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, gad and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class QuantitiesTable(Document):
+	pass
